8ca532c0213ad0da9580fe2b88c0ca783d9e9f0a
trineob <trineob@kth.se>
2025-11-17 13:18:29 +0100

    Change

--- interaction-1.js
+++ interaction-1.js
@@ -60,7 +60,7 @@ function rotationChange(rotx, roty, rotz) {
 }
 
 function mousePressed() {
-    //playAudio()
+    playAudio()
     // Use this for debugging from the desktop!
 }
 
--- interaction-2.js
+++ interaction-2.js
@@ -12,7 +12,7 @@ let dspNodeParams = null;
 let jsonParams = null;
 
 // Change here to ("tuono") depending on your wasm file name
-const dspName = "bells";
+const dspName = "churchbell";
 const instance = new FaustWasm2ScriptProcessor(dspName);
 
 // output to window or npm package module
@@ -25,7 +25,7 @@ if (typeof module === "undefined") {
 }
 
 // The name should be the same as the WASM file, so change tuono with brass if you use brass.wasm
-bells.createDSP(audioContext, 1024)
+churchbell.createDSP(audioContext, 1024)
     .then(node => {
         dspNode = node;
         dspNode.connect(audioContext.destination);
