f90c02942e56da2a37e6fc0d550d599204eafc55
trineob <trineob@kth.se>
2025-11-17 13:29:46 +0100

    tiemout

--- interaction-1.js
+++ interaction-1.js
@@ -112,7 +112,7 @@ function playAudio() {
     // For example if you change to a bell sound, here you could use "/churchBell/gate" instead of
     // "/thunder/rumble".
     dspNode.setParamValue("/torpedo/trigger", 1)
-    //setTimeout(() => { dspNode.setParamValue("/torpedo/trigger", 0) }, 100);
+    setTimeout(() => { dspNode.setParamValue("/torpedo/trigger", 0) }, 100);
 }
 
 //==========================================================================================
