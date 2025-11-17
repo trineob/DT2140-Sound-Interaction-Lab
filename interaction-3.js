11ee4d85118f40a9c38469195a5714b9907841a3
trineob <trineob@kth.se>
2025-11-17 13:00:03 +0100

    Update

--- interaction-3.js
+++ interaction-3.js
@@ -74,7 +74,7 @@ function deviceTurned() {
 function deviceShaken() {
     shaketimer = millis();
     statusLabels[0].style("color", "pink");
-    playAudio();
+    // playAudio();
 }
 
 function getMinMaxParam(address) {
